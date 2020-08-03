47ad90834e70bc2d4f7b2cc63230aabf6e55f63a
Nohossat TRAORE <nohossat.traore@gmail.com>
2020-08-03 10:06:04 +0200

    fix JS behaviour

--- chatbot_app/src/static/js/main.js
+++ chatbot_app/src/static/js/main.js
@@ -1,23 +1,31 @@
+// add click listener on topic buttons
 let topics = document.getElementsByClassName('btn-topic');
 
 for (let topic of topics) {
     topic.addEventListener('click', function(e){
         class_items = e.target.className.split(' ');
-        questions = document.getElementsByClassName("topic_questions " + class_items[3]);
+        question = document.querySelector(".topic_questions." + class_items[3]);
 
         for (let top of document.getElementsByClassName('topic_questions')) {
             top.style.display = "none";
         }
 
-        questions[0].style.display = "block";
+        question.style.display = "block";
     })
 }
 
-let responses = document.getElementsByClassName('topic_responses');
+// add click listener for the questions per topic
+let questions = document.getElementsByClassName('question_faq');
 
-for (let resp of responses) {
-    resp.addEventListener('click', function(e){
-        console.log('test')
-        e.target.style.display = "block";
+for (let question of questions) {
+    question.addEventListener('click', function(e){
+        class_items = e.target.className.split(' ');
+        response = document.querySelector(".response_faq." + class_items[1] + "." + class_items[2]);
+
+        for (let element of document.getElementsByClassName('response_faq')) {
+            element.style.display = "none";
+        }
+
+        response.style.display = "block";
     })
 }
